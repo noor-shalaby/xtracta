9962f123557917d5cf72e2d6ab32d286c938137e
Noor Shalaby <ono.shalaby@gmail.com>
2026-01-26 15:11:02 +0200

    write expanding and collapsing logic each into its own function

--- src/file_button.gd
+++ src/file_button.gd
@@ -11,16 +11,23 @@ var is_expanded: bool = false
 func _on_pressed() -> void:
 	match is_expanded:
 		false:
-			reset_tween()
-			tween.tween_property(self, "custom_minimum_size:y", default_min_height + 512, 0.2)
-			is_expanded = true
+			expand(default_min_height + 512)
 		true:
-			reset_tween()
-			tween.tween_property(self, "custom_minimum_size:y", default_min_height, 0.2)
-			is_expanded = false
+			collapse()
 
 
 func reset_tween() -> void:
 	if tween:
 		tween.kill()
 	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+
+
+func expand(target_size: float) -> void:
+	reset_tween()
+	tween.tween_property(self, "custom_minimum_size:y", target_size, 0.2)
+	is_expanded = true
+
+func collapse() -> void:
+	reset_tween()
+	tween.tween_property(self, "custom_minimum_size:y", default_min_height, 0.2)
+	is_expanded = false
